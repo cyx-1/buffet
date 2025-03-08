08eb16ad2d8291a1589bd9c18483a45ffaf70367
Dave Chen <yexinchen@gmail.com>
2025-03-08 12:35:33 -0500

    testing sonar2

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import util
+import sys
+
+
+def func():
+    pass
+
 
 if __name__ == '__main__':
+    foo = 1 == "1"
+    # TODO : something
     test = 'hello world'
     url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?id=UNRATE&cosd=1948-01-01&fq=Monthly'
     folder_path = 'c:\\Users\\yexin\\OneDrive\\PDAJ\\Yexin\\Finance\\Data'
