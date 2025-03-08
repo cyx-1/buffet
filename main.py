3b304768f8715c966e0b17d44466b477125cc5fe
Dave Chen <yexinchen@gmail.com>
2025-03-08 12:22:19 -0500

    testing sonar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import util
 
 if __name__ == '__main__':
+    test = 'hello world'
     url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?id=UNRATE&cosd=1948-01-01&fq=Monthly'
     folder_path = 'c:\\Users\\yexin\\OneDrive\\PDAJ\\Yexin\\Finance\\Data'
     file_name = 'unemployment-monthly.csv'
